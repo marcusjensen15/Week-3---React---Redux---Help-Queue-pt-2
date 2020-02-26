d95a82ec336433e886bd15323cae2f632c986cdd
marcusjensen15 <marcusjensen1515@gmail.com>
2020-02-26 15:35:46 -0800

    update time function now working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,7 @@ class App extends React.Component {
   componentDidMount() {
     this.waitTimeUpdateTimer = setInterval(() =>
       this.updateTicketElapsedWaitTime(),
-    60000
+    6000
     );
   }
 
@@ -31,12 +31,25 @@ class App extends React.Component {
   }
 
   updateTicketElapsedWaitTime() {
+    const { dispatch } = this.props;
+    Object.keys(this.props.masterTicketList).map(ticketId => {
+    const ticket = this.props.masterTicketList[ticketId];
+    const newFormattedWaitTime = ticket.timeOpen.fromNow(true);
+    const action = {
+
+      type: 'UPDATE_TIME',
+      id: ticketId,
+      formattedWaitTime: newFormattedWaitTime
+    };
+    dispatch(action);
+
     // var newMasterTicketList = Object.assign({}, this.state.masterTicketList);
     // Object.keys(newMasterTicketList).forEach(ticketId => {
     //   newMasterTicketList[ticketId].formattedWaitTime = (newMasterTicketList[ticketId].timeOpen).fromNow(true);
     // });
     // this.setState({masterTicketList: newMasterTicketList});
-  }
+  });
+}
 
   handleChangingSelectedTicket(ticketId){
     this.setState({selectedTicket: ticketId});
